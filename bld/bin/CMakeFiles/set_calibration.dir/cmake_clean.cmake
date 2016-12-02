b1df61d84bf25d3f1272779c381297c7d81e6352
ben <ben.spoor@gmail.com>
2016-12-02 22:21:44 +0100

    Ignore bld dir

--- bld/bin/CMakeFiles/set_calibration.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "toggle-led-atmega16.map"
-  "CMakeFiles/set_calibration"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/set_calibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
