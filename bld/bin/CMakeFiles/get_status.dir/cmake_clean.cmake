a2bcdf0626b67ed1fc522820b7de1d38ac98aed4
ben <ben.spoor@gmail.com>
2016-12-03 14:10:26 +0100

    Removed bld

--- bld/bin/CMakeFiles/get_status.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "toggle-led-atmega16.map"
-  "CMakeFiles/get_status"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/get_status.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- bld/bin/CMakeFiles/set_fuses.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "toggle-led-atmega16.map"
-  "CMakeFiles/set_fuses"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/set_fuses.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
