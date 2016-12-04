77b6b3e1789c0eff258849656e2d08d5a27e3f0c
ben <ben.spoor@gmail.com>
2016-12-04 22:00:19 +0100

    Introduce move and serial lib

---
+++ inc/move.h
@@ -0,0 +1,7 @@
+
+void move_init(void);
+void move_stop(void);
+void move_forward(void);
+void move_left(void);
+void move_backward(void);
+void move_right(void);
---
+++ lib/move.c
@@ -0,0 +1,37 @@
+#include <avr/io.h>
+
+#include "move.h"
+
+/**
+  \brief Initialization of Movement library
+*/
+void move_init(void)
+{
+    //todo
+}
+
+void move_stop(void)
+{
+    //todo
+}
+
+void move_forward(void)
+{
+    //todo
+}
+
+void move_left(void)
+{
+    //todo
+}
+
+void move_backward(void)
+{
+    //todo
+}
+
+void move_right(void)
+{
+    //todo
+}
+
