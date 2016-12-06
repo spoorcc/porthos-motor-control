7325c9ce771db640f357a477a0fcb1209039ee55
ben <ben.spoor@gmail.com>
2016-12-06 23:07:05 +0100

    Set proper UBRR for serial communication

--- inc/serial.h
+++ inc/serial.h
@@ -1,9 +1,6 @@
-/** Clock speed */
-#define FOSC 1843200 
-
 /** Baud rate */
 #define BAUD 9600
-#define MYUBRR FOSC/16/BAUD-1
+#define MYUBRR F_CPU/(16UL*BAUD)-1
 
 void serial_init(unsigned int ubrr);
 
